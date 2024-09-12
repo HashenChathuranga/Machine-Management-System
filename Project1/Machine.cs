49dedf07c2b18813ef8428cb85e64695cec41c1a
HashenChathuranga <hashen.chathuranga98@gmail.com>
2024-09-13 00:22:17 +0530

    initial project setup

---
+++ Project1/Machine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public class Machine
+    {
+        public string MachineID { get; set; }
+        public string MachineName { get; set; }
+        public string MachineUsageStage { get; set; }
+        public string ResponsiblePerson { get; set; }
+        public int NumberOfEmployees { get; set; }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+
+        public struct Credentials
+        {
+            public static string UserID;
+            public static string UserName;
+            public static string Location;
+            public static string Role;
+        }
+
+        public static string getConnectionSrtring()
+        {
+            return "server=localhost;uid=root;password=1234;database=machine_management_sys;";
+        }
+    }
+}
